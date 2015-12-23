e018e2f292340fc84223c9ad35e0c7084994a0e4
wolfram74 <phaugen@gmail.com>
2015-12-23 12:51:45 -0600

    templates all work, populate json now

--- schmutz_jager/scripts/main.js
+++ schmutz_jager/scripts/main.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
-
-  console.log('doc ready')
+  var thisWeek = makeActive(weekNum());
+  var row = $('<tr></tr>')
+  Handlebars.registerPartial('taskPartial', $('#taskTemplate').html())
+  var spaceTemplate = Handlebars.compile($('#spaceTemplate').html())
+  for(var room in thisWeek){
+    var cell = $(spaceTemplate(thisWeek[room]))
+    console.log(cell)
+    row.append(cell)
+  };
+  $('table').append(row)
 })
 console.log('get eet done')
+
+function weekNum(){
+  var monday = new Date(2007, 0, 1);
+  var now = new Date();
+  var delta = now.valueOf() - monday.valueOf();
+  var weekMiliSeconds = 7*24*3600*1000;
+  return parseInt(delta/weekMiliSeconds);
+}
+
+function makeActive(number){
+  var cycle = number % 4;
+  var activeSpaces = {};
+  var offSet = 0;
+  for(var room in spaces){
+    var residentNumber = (number+offSet)%3
+    activeSpaces[room] = {person: residents[residentNumber], tasks: [], location: room}
+    for(var i =0; i < spaces[room].tasks.length; i++){
+      var task = spaces[room].tasks[i];
+      if(cycle%task.weekPeriod === 0){
+        activeSpaces[room].tasks.push(task)
+      };
+    };
+    offSet+=1
+  }
+  return activeSpaces
+}
--- schmutz_jager/scripts/spaces.js
+++ schmutz_jager/scripts/spaces.js
@@ -4,9 +4,22 @@ var spaces = {
       {
         title: 'sweep',
         weekPeriod: 1,
-        description:'',
-        completionSigns:''
+        description:'Use a broom or vacuum cleaner to get clean the floor',
+        completionSigns:"You've swept over the floor and moved furniture at some point, it's hard to see hair while standing."
       },
+      {
+        title: 'Clear table',
+        weekPeriod: 1,
+        description:'Get the coffee table clear of clutter',
+        completionSigns: "table is clear of papers, chords and computers"
+      },
+      {
+        title: 'Wipe down table',
+        weekPeriod: 2,
+        description:'Using a moist cloth wipe down table',
+        completionSigns: "You "
+      },
+
     ]
   },
   kitchen: {
@@ -51,4 +64,19 @@ var spaces = {
   }
 }
 
+var residents = ["Cari","Peter","Ryan"]
 console.log('tasks loaded')
+/*
+
+http://www.allyou.com/budget-home/organizing-cleaning/fast-track-your-cleaning-routine
+
+http://3.bp.blogspot.com/-K5cPs43ZjTc/Tud1VRlRs2I/AAAAAAAABGg/PRU73yf2x7Q/s1600/Slide1.GIF
+
+towards bottom
+http://happymoneysaver.com/my-ultimate-happy-home-cleaning-routine-plus-free-printable-checklist/
+
+http://www.cleanandscentsible.com/2014/01/developing-daily-cleaning-schedule.html
+
+look at that, not only is the problem solved, surveys have been done of solution space
+http://piganddac.com/a-pinterest-roundup-of-cleaning-routines/
+*/
