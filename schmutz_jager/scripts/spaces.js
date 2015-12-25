cfdfa26bbd4254f2c631dcaf54d3af0a78912913
wolfram74 <phaugen@gmail.com>
2015-12-24 23:46:28 -0600

    more tasks added to the spaces object

--- schmutz_jager/scripts/spaces.js
+++ schmutz_jager/scripts/spaces.js
@@ -30,6 +30,30 @@ var spaces = {
         description:'Use a broom or vacuum cleaner to get clean the floor',
         completionSigns:"You've swept over the floor and moved furniture at some point, it's hard to see hair while standing."
       },
+      {
+        title: 'Clear counters',
+        weekPeriod: 1,
+        description:'Get the various counters clear of clutter',
+        completionSigns: "You could put the big cutting board down on any of the portion of counter big enough to use it, produce with signs of rot have been composted."
+      },
+      {
+        title: 'Wipe down counters',
+        weekPeriod: 1,
+        description:'Using a moist cloth wipe down counters',
+        completionSigns: "You got something moist at some point."
+      },
+      {
+        title: 'Clean dishes in the sink',
+        weekPeriod: 1,
+        description:'Make sure the sink is empty of dirty dishes at some point in the week',
+        completionSigns: "dishes were cleaned."
+      },
+      {
+        title: 'Move dried dishes to where they belong',
+        weekPeriod: 1,
+        description:'self explanatory',
+        completionSigns: "The drying wrack is empty."
+      },
     ]
   },
   bathRoom: {
@@ -40,6 +64,18 @@ var spaces = {
         description:'Use a broom or vacuum cleaner to get clean the floor',
         completionSigns:"You've swept over the floor and moved furniture at some point, it's hard to see hair while standing."
       },
+      {
+        title: 'scrub toilet',
+        weekPeriod: 1,
+        description:'Use toilet brush to remove water stain on bowl.',
+        completionSigns:"You've gotten the brush wet and at least the upper portion of the bowl is clean."
+      },
+      {
+        title: 'wipe down sink',
+        weekPeriod: 1,
+        description:'Use some towel or brush to clean off some of the residue on the sink. relocate objects to ensure you get all the surfaces.',
+        completionSigns:"You've gotten something wet and there are no visible stains on the surface."
+      },
     ]
   },
   diningSpace: {
@@ -49,6 +85,18 @@ var spaces = {
         weekPeriod: 1,
         description:'Use a broom or vacuum cleaner to get clean the floor',
         completionSigns:"You've swept over the floor and moved furniture at some point, it's hard to see hair while standing."
+      },
+      {
+        title: 'Clear table',
+        weekPeriod: 1,
+        description:'Get the dinner table clear of clutter',
+        completionSigns: "table is clear of papers, chords and computers"
+      },
+      {
+        title: 'Wipe down table',
+        weekPeriod: 2,
+        description:'Using a moist cloth wipe down table',
+        completionSigns: "You got something moist at some point."
       },
     ]
   },
