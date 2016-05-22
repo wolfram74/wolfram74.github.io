8786cb4a8c3f24038cb8c91bf832dd71577c0993
wolfram74 <phaugen@gmail.com>
2016-05-22 11:54:52 -0500

    made physics article template safer

--- templates/article_template/scripts/main.js
+++ templates/article_template/scripts/main.js
@@ -1,7 +1,9 @@
 $(document).on('ready', function(){
   console.log('ready')
-  MathJax.Hub.Config({
-      tex2jax: {inlineMath: [["$&","&$"]]}
-    });
+  if(typeof MathJax !== 'undefined'){
+    MathJax.Hub.Config({
+        tex2jax: {inlineMath: [["$&","&$"]]}
+      });
+  }
 });
 
