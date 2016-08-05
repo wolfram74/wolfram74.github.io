bcbf3e27cbf4b764e7b183b7037d6613775306d3
wolfram74 <phaugen@gmail.com>
2016-08-05 16:22:17 -0500

    made word count tool

---
+++ scripts/wordcount/main.js
@@ -0,0 +1,17 @@
+console.log('linked')
+$('document').ready(function(){
+  $('textarea').on('keyup', areaHandler)
+})
+
+var areaHandler = function(event){
+  var text = event.target.value
+  console.log(text)
+  var spaces = text.split(' ').length
+  $('#present').attr('value', spaces)
+  var progress = spaces / $('#goal').attr('value')
+  if( progress > 1){
+    $('input').css('border', 'solid 5px #0f0')
+  } else if(progress > .5){
+    $('input').css('border', 'solid 5px #077')
+  }
+}
