3cca4617ade3cb580ec28199adb6d1397136daa3
wolfram74 <phaugen@gmail.com>
2020-01-11 12:52:33 -0600

    just catching stuff up?

---
+++ physics_articles/011_custom_units/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function(){
+  console.log('ready')
+  if(typeof MathJax !== 'undefined'){
+    MathJax.Hub.Config({
+        tex2jax: {inlineMath: [["$&","&$"]]}
+      });
+  }
+});
